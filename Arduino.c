a02d1e19706b8ea28a7fc9ed8da004d9f9455360
Jainish Mehta <45054638+jainishmehta@users.noreply.github.com>
2021-04-30 01:38:21 -0400

    Update Arduino.c

--- Arduino.c
+++ Arduino.c
@@ -1,31 +1,29 @@
-/*     Arduino Rotary Encoder 
- *  
- */
+// Arduino Rotary Encoder 
  
  #define output1 5
  #define output2 6
  int counter = 0; 
  int aState;
- int aLastState;  
+ int alastState;  
  void setup() { 
    pinMode (output1,INPUT);
    pinMode (output2,INPUT);
    
    Serial.begin (1000);
-   // Reads the initial state of the outputA
-   aLastState = digitalRead(outputA);   
+   // Reads the initial state of the first output
+   aLastState = digitalRead(output1);   
  } 
- void loop() { 
-   aState = digitalRead(outputA); // Reads current state of the outputA
-   // If the previous != current state of the outputA, Pulse has occured
+ void main() { 
+  // Get current state of the first output
+   aState = digitalRead(output1); 
+   // If the previous is not the same as the current state of the first output
    if (aState != aLastState){     
      // If the outputB != the outputA state, encoder is rotating clockwise
-     if (digitalRead(outputB) != aState) { 
-       counter ++;
-     } else {
+     if (digitalRead(outputB) == aState) { 
        counter --;
+     } else {
+       counter ++;
      }
-     Serial.print("Position: ");
-     Serial.println(counter);
    } 
-   aLastState = aState; // Updates the previous state of outputA
+  // Updates the previous state of frst output
+   aLastState = aState; 
